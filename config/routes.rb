fd796359fef95267be8a225b2840ab1be1fcd664
Juyeong Lee <2jy22@naver.com>
2021-07-19 23:52:21 +0900

    Framework7 searchbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  root to: 'maps#index'
 
   resources :home, only: :index
   resources :maps, only: :index
