ced2d93974810d62133e25dc5b74ab1ce71a3a18
Juyeong Lee <2jy22@naver.com>
2021-07-20 21:36:46 +0900

    Display elements

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -1,4 +1,6 @@
 class ElementsController < ApplicationController
   def index
+    # @elements = Element.order('name collate "C" asc')
+    @elements = Element.all.sort_by { |element| element.name }
   end
 end
