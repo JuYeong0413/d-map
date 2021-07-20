a72e80482587b24577cbac71cce4066b4567c68d
Juyeong Lee <2jy22@naver.com>
2021-07-20 16:19:49 +0900

    Elements model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_20_065259) do
+ActiveRecord::Schema.define(version: 2021_07_20_070043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,17 @@ ActiveRecord::Schema.define(version: 2021_07_20_065259) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "elements", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.decimal "lat"
+    t.decimal "lng"
+    t.string "instagram_id"
+    t.string "link"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
