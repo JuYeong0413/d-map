bf13ee231b04195306f3a4f4aa46fbce8a861b53
Juyeong Lee <2jy22@naver.com>
2021-08-05 03:11:31 +0900

    Add kakao AdFit

--- app/javascript/packs/popup.js
+++ app/javascript/packs/popup.js
@@ -94,6 +94,12 @@ export function createDynamicPopup(elementId) {
                     </span>
                   </div>
 
+                  <ins class="kakao_ad_area" style="display:none;"
+                   data-ad-unit    = "DAN-rvmbyxxoTLIUW3zc"
+                   data-ad-width   = "320"
+                  data-ad-height  = "50"></ins>
+                  <script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>
+
                 </div>
               </div>
 
