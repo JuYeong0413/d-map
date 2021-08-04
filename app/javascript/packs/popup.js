0e42efffb0b4d064e83ef4bcbecadc284030322c
Juyeong Lee <2jy22@naver.com>
2021-08-05 03:24:32 +0900

    Update kakao AdFit banner size

--- app/javascript/packs/popup.js
+++ app/javascript/packs/popup.js
@@ -97,7 +97,7 @@ export function createDynamicPopup(elementId) {
                   <ins class="kakao_ad_area" style="display:none;"
                    data-ad-unit    = "DAN-rvmbyxxoTLIUW3zc"
                    data-ad-width   = "320"
-                  data-ad-height  = "50"></ins>
+                  data-ad-height  = "100"></ins>
                   <script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>
 
                 </div>
