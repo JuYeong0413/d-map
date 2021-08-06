3fb95de7290de020f5776cc785df0be4071d46ae
Juyeong Lee <2jy22@naver.com>
2021-08-06 22:02:44 +0900

    Enable Sentry only in production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,7 @@
 Sentry.init do |config|
   config.dsn = ENV['SENTRY_DSN']
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.enabled_environments = %w[production]
 
   # Set tracesSampleRate to 1.0 to capture 100%
   # of transactions for performance monitoring.
